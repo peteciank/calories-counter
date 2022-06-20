a5a951bc1b19e780e60675234ce89d84e8585dc0
peteciank <106826936+peteciank@users.noreply.github.com>
2022-06-20 01:14:33 -0300

    Update hello_world_v1.py

--- hello_world_v1.py
+++ hello_world_v1.py
@@ -1,7 +1,32 @@
 import streamlit as st
+import pandas as pd
+
+# Creating Data Frame 1
+data = {
+  "calories": [420, 380, 390],
+  "duration": [50, 40, 45]
+}
+
+df1 = pd.DataFrame(data)
+
+
 
 st.write("Hello World, this is my new Streamlit application. It works again. ")
 
+element = st.dataframe(df1)
+
+
 with st.form(key='my_form'):
- 	text_input = st.text_input(label='Enter some text')
+ 	calories = st.text_input(label='Enter calories')
+  duration = st.text_input(label='Enter duration')
+  
+  data = {
+   "calories": [calories],
+   "duration": [duration]
+  }
+  
+  df2 = pd.DataFrame(data)
+  element.add_rows(df2)
+
+  
  	submit_button = st.form_submit_button(label='Submit')
