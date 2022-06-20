eec4fc7c89b1a6a037c37ee871d3085b954a58c8
peteciank <106826936+peteciank@users.noreply.github.com>
2022-06-20 01:06:35 -0300

    Update hello_world_v1.py

--- hello_world_v1.py
+++ hello_world_v1.py
@@ -1,3 +1,7 @@
 import streamlit as st
 
 st.write("Hello World, this is my new Streamlit application. It works again. ")
+
+with st.form(key='my_form'):
+ 	text_input = st.text_input(label='Enter some text')
+ 	submit_button = st.form_submit_button(label='Submit')
